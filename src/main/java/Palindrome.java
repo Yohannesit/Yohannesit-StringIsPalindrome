6e1ef1a6c98b8ef6f46c41e46aba707502759a26
Yohannes Itanna <itannayohannes@gmail.com>
2025-04-23 08:35:35 +0000

    Completed Lab String Is Palindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,16 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        if (str == null || str.length() == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < str.length()/2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+
+        }
+        return true;
     }
 }
